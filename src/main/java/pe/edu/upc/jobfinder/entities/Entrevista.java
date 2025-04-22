8dcf692296c0042a62780f06f2b1e5cc34c57f97
MiguelGuevara25 <miguel_g1789@hotmail.com>
2025-04-22 15:46:09 -0500

    completed tables miss FK

--- src/main/java/pe/edu/upc/jobfinder/entities/Entrevista.java
+++ src/main/java/pe/edu/upc/jobfinder/entities/Entrevista.java
@@ -1,13 +1,23 @@
 package pe.edu.upc.jobfinder.entities;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+@Entity
+@Table(name = "Entrevista")
 public class Entrevista {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(name = "date", nullable = false)
     private LocalDate date;
+    @Column(name = "hour", nullable = false)
     private LocalTime hour;
+    @Column(name = "modality", nullable = false, length = 100)
     private String modality;
+    @Column(name = "result", nullable = false, length = 100)
     private String result;
 
     //private Usuario usuario;
