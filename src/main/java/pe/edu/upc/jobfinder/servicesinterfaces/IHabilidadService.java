64cdbd86d066f66a86c20755c4a44c60e36cd4cf
G4TUNO <u202219154@upc.edu.pe>
2025-04-22 18:33:40 -0500

    Se completo serviceinterfaces de la tabla Habilidad

---
+++ src/main/java/pe/edu/upc/jobfinder/servicesinterfaces/IHabilidadService.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.jobfinder.servicesinterfaces;
+
+import pe.edu.upc.jobfinder.entities.Habilidad;
+
+import java.util.List;
+
+public interface IHabilidadService {
+    public List<Habilidad> list();
+    public void insert(Habilidad h);
+    public Habilidad searchId(int id);
+    public void update(Habilidad h);
+    public void delete(int id);
+}
