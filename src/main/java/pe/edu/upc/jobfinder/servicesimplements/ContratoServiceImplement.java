e3207f77cf6f5243afcd5823110282d11ae8b43f
MiguelGuevara25 <miguel_g1789@hotmail.com>
2025-04-22 15:21:27 -0500

    implement contrato completed

--- src/main/java/pe/edu/upc/jobfinder/servicesimplements/ContratoServiceImplement.java
+++ src/main/java/pe/edu/upc/jobfinder/servicesimplements/ContratoServiceImplement.java
@@ -1,35 +1,40 @@
 package pe.edu.upc.jobfinder.servicesimplements;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pe.edu.upc.jobfinder.entities.Contrato;
+import pe.edu.upc.jobfinder.repositories.IContratoRepository;
 import pe.edu.upc.jobfinder.servicesinterfaces.IContratoService;
 
 import java.util.List;
 
 @Service
 public class ContratoServiceImplement implements IContratoService {
+    @Autowired
+    private IContratoRepository cR;
+
     @Override
     public List<Contrato> list() {
-        return List.of();
+        return cR.findAll();
     }
 
     @Override
     public void insert(Contrato contrato) {
-
+        cR.save(contrato);
     }
 
     @Override
     public Contrato searchId(int id) {
-        return null;
+        return cR.findById(id).orElse(new Contrato());
     }
 
     @Override
     public void update(Contrato contrato) {
-
+        cR.save(contrato);
     }
 
     @Override
     public void delete(int id) {
-
+        cR.deleteById(id);
     }
 }
