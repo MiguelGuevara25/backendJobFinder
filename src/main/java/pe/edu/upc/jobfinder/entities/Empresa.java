cf0a22bb9b3a7ab4131cbd5b64f40e3b1db3b729
JhampierCD <U201917576@upc.edu.pe>
2025-04-22 20:03:09 -0500

    Empresa Intities y Repository

--- src/main/java/pe/edu/upc/jobfinder/entities/Empresa.java
+++ src/main/java/pe/edu/upc/jobfinder/entities/Empresa.java
@@ -8,8 +8,22 @@ public class Empresa {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @Column(name = "name", nullable = false, length = 20)
+    @Column(name = "namecompany", nullable = false, length = 100)
     private int name;
     @Column(name = "description", nullable = false, length = 100)
     private String description;
+    @Column(name = "sector", nullable = false, length = 100)
+    private String sector;
+    @Column(name = "website", nullable = false, length = 100)
+    private String website;
+    @Column(name = "address", nullable = false, length = 100)
+    private String address;
+    @Column(name = "telephone", nullable = false, length = 100)
+    private double telephone;
+    @Column(name = "mail", nullable = false, length = 100)
+    private String mail;
+    @Column(name = "location", nullable = false, length = 100)
+    private String location;
+    @Column(name = "password", nullable = false, length = 100)
+    private String password;
 }
--- src/main/java/pe/edu/upc/jobfinder/repositories/IEmpresaRepository.java
+++ src/main/java/pe/edu/upc/jobfinder/repositories/IEmpresaRepository.java
@@ -3,5 +3,8 @@ package pe.edu.upc.jobfinder.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pe.edu.upc.jobfinder.entities.Empresa;
 
+import java.util.List;
+
 public interface IEmpresaRepository extends JpaRepository<Empresa, Integer> {
+
 }
