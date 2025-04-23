3f99400096a52264f6cf9564b63d8686ad71e384
JhampierCD <U201917576@upc.edu.pe>
2025-04-22 19:25:33 -0500

    Se creo las entidades de las tablas Empresa, Postulacion y Oferta de Trabajo. Ademàs se creo el service interfaces ( Empresa).

---
+++ src/main/java/pe/edu/upc/jobfinder/controllers/OfertadeTrabajoController.java
@@ -0,0 +1,4 @@
+package pe.edu.upc.jobfinder.controllers;
+
+public class OfertadeTrabajoController {
+}
---
+++ src/main/java/pe/edu/upc/jobfinder/servicesinterfaces/IEmpresaService.java
@@ -0,0 +1,14 @@
+package pe.edu.upc.jobfinder.servicesinterfaces;
+
+
+import pe.edu.upc.jobfinder.entities.Empresa;
+
+import java.util.List;
+
+public interface IEmpresaService {
+    public List<Empresa> list();
+    public void insert(Empresa empresa);
+    public Empresa searchId(int id);
+    public void update(Empresa Empresa);
+    public void delete(int id);
+}
