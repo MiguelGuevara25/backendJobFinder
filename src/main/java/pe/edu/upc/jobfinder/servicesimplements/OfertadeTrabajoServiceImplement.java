3d109bbacb8f9f98493b9b20a9835b0789237d7b
JhampierCD <U201917576@upc.edu.pe>
2025-04-22 20:45:49 -0500

    Oferta de trabajo Sevice Implements

--- src/main/java/pe/edu/upc/jobfinder/servicesimplements/OfertadeTrabajoServiceImplement.java
+++ src/main/java/pe/edu/upc/jobfinder/servicesimplements/OfertadeTrabajoServiceImplement.java
@@ -1,4 +1,36 @@
 package pe.edu.upc.jobfinder.servicesimplements;
 
-public class OfertadeTrabajoServiceImplement {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pe.edu.upc.jobfinder.entities.OfertadeTrabajo;
+import pe.edu.upc.jobfinder.repositories.IOfertadeTrabajoRepository;
+import pe.edu.upc.jobfinder.servicesinterfaces.IOfertadeTrabajoService;
+
+import java.util.List;
+
+@Service
+public class OfertadeTrabajoServiceImplement implements IOfertadeTrabajoService {
+    @Autowired
+    private IOfertadeTrabajoRepository oR;
+
+    @Override
+    public List<OfertadeTrabajo> list() {return oR.findAll();}
+
+    @Override
+    public void insert(OfertadeTrabajo ofertadetrabajo) {oR.save(ofertadetrabajo);}
+
+    @Override
+    public OfertadeTrabajo searchId(int id) {return oR.findById(id).orElse(new OfertadeTrabajo());}
+
+    @Override
+    public void update(OfertadeTrabajo ofertadeTrabajo) {oR.save(ofertadeTrabajo);}
+
+    @Override
+    public void delete(int id) {oR.deleteById(id);}
+
+    @Override
+    public List<OfertadeTrabajo> search(String name) {
+        return List.of();
+    }
 }
+
