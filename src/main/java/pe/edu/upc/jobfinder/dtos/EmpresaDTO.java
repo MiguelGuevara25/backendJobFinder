72fc9101805f851df5484de0ca6e8d0a0231c90e
JhampierCD <U201917576@upc.edu.pe>
2025-04-22 20:21:49 -0500

    Empresa Inttities, controller,DTO, Repository y Service

--- src/main/java/pe/edu/upc/jobfinder/dtos/EmpresaDTO.java
+++ src/main/java/pe/edu/upc/jobfinder/dtos/EmpresaDTO.java
@@ -1,4 +1,100 @@
 package pe.edu.upc.jobfinder.dtos;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class EmpresaDTO {
+
+    private int id;
+    private int name;
+    private String description;
+    private String sector;
+    private String website;
+    private String address;
+    private double telephone;
+    private String mail;
+    private String location;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getName() {
+        return name;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSector() {
+        return sector;
+    }
+
+    public void setSector(String sector) {
+        this.sector = sector;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(double telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- src/main/java/pe/edu/upc/jobfinder/repositories/IEmpresaRepository.java
+++ src/main/java/pe/edu/upc/jobfinder/repositories/IEmpresaRepository.java
@@ -1,10 +1,13 @@
 package pe.edu.upc.jobfinder.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pe.edu.upc.jobfinder.entities.Empresa;
 
 import java.util.List;
 
 public interface IEmpresaRepository extends JpaRepository<Empresa, Integer> {
-
+    @Query("Select a from App a where a.nameApp like %:nombre%")
+    public List<App> buscar(@Param("nombre") String nombre);
 }
