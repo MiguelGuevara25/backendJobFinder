156bad7a66f018aeb500c488d26522323a3cb9b7
PaToxCo <rodrigoanayaquispe1206@gmail.com>
2025-04-23 17:16:12 -0500

    Implementacion de entidad Curriculum - CRUD - COMPLETO

--- src/main/java/pe/edu/upc/jobfinder/controllers/CurriculumController.java
+++ src/main/java/pe/edu/upc/jobfinder/controllers/CurriculumController.java
@@ -3,10 +3,11 @@ package pe.edu.upc.jobfinder.controllers;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.jobfinder.dtos.ContratoDTO;
 import pe.edu.upc.jobfinder.dtos.CurriculumDTO;
+import pe.edu.upc.jobfinder.entities.Contrato;
+import pe.edu.upc.jobfinder.entities.Curriculum;
 import pe.edu.upc.jobfinder.serviceinterfaces.ICurriculumService;
 
 import java.util.List;
@@ -24,5 +25,30 @@ public class CurriculumController {
             ModelMapper modelMapper = new ModelMapper();
             return modelMapper.map(curriculum, CurriculumDTO.class);
         }).collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public void insertar(@RequestBody ContratoDTO dto) {
+        ModelMapper modelMapper = new ModelMapper();
+        Curriculum curriculum = modelMapper.map(dto, Curriculum.class);
+        curriculumService.insert(curriculum);
+    }
+
+    @GetMapping("/{id}")
+    public ContratoDTO listarId(@PathVariable int id) {
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper.map(curriculumService.buscarID(id), ContratoDTO.class);
+    }
+
+    @PutMapping
+    public void modificar(@RequestBody ContratoDTO dto) {
+        ModelMapper m = new ModelMapper();
+        Curriculum curriculum = m.map(dto, Curriculum.class);
+        curriculumService.update(curriculum);
+    }
+
+    @DeleteMapping({"/{id}"})
+    public void eliminar(@PathVariable int id) {
+        curriculumService.delete(id);
     }
 }
