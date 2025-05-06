13905f97aa5e8bc82be8db7cc86cf5323623f205
JhampierCD <U201917576@upc.edu.pe>
2025-05-06 15:39:08 -0500

    Querys E

--- src/main/java/pe/edu/upc/jobfinder/repositories/IOfertadeTrabajoRepository.java
+++ src/main/java/pe/edu/upc/jobfinder/repositories/IOfertadeTrabajoRepository.java
@@ -1,8 +1,17 @@
 package pe.edu.upc.jobfinder.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pe.edu.upc.jobfinder.entities.OfertadeTrabajo;
-
+import java.util.List;
 public interface IOfertadeTrabajoRepository extends JpaRepository<OfertadeTrabajo, Integer> {
+
+    @Query("SELECT o FROM OfertadeTrabajo o WHERE o.salary >= :salarioMinimo AND o.location = :ubicacion")
+    public List<OfertadeTrabajo> buscarOfertasPorSalarioYUbicacion(@Param("salarioMinimo") double salarioMinimo, @Param("ubicacion") String ubicacion);
+    @Query("SELECT o.name, o.salary, o.typeofcontract, e.name " +
+            "FROM OfertadeTrabajo o JOIN o.empresa e " +
+            "WHERE o.location = :ubicacion AND o.typeofcontract = :tipoContrato")
+    public List<String[]> buscarOfertasPorUbicacionYContratoConEmpresa(@Param("ubicacion") String ubicacion, @Param("tipoContrato") String tipoContrato);
 
 }
--- src/main/java/pe/edu/upc/jobfinder/repositories/IPostulacionRepository.java
+++ src/main/java/pe/edu/upc/jobfinder/repositories/IPostulacionRepository.java
@@ -1,9 +1,22 @@
 package pe.edu.upc.jobfinder.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upc.jobfinder.entities.OfertadeTrabajo;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pe.edu.upc.jobfinder.entities.Postulacion;
 
+import java.util.List;
+
 public interface IPostulacionRepository extends JpaRepository<Postulacion, Integer> {
+
+    @Query(value = "SELECT p.state, COUNT(*) AS cantidad_postulaciones " +
+            "FROM Postulacion p " +
+            "WHERE p.acceptedcandidate = :aceptado " +
+            "GROUP BY p.state", nativeQuery = false)
+    public List<String[]> contarPostulacionesPorEstado(@Param("aceptado") Boolean aceptado);
+
+    @Query("SELECT p FROM Postulacion p WHERE p.acceptedcandidate = :aceptado")
+    public List<Postulacion> buscarPostulacionesPorAceptacion(@Param("aceptado") Boolean aceptado);
+
 
 }
