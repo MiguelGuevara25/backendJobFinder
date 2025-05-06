ed36d80a323df933dde15eceefdd3487bfbedf07
G4TUNO <u202219154@upc.edu.pe>
2025-05-06 15:42:50 -0500

    Informacion adicional de los queries

--- src/main/java/pe/edu/upc/jobfinder/repositories/IExperienciaRepository.java
+++ src/main/java/pe/edu/upc/jobfinder/repositories/IExperienciaRepository.java
@@ -9,7 +9,10 @@ import java.util.List;
 
 @Repository
 public interface IExperienciaRepository extends JpaRepository<Experiencia, Integer> {
-    //Ver qué puestos implican mayor permanencia (más estables)
+    //1. Este query calcula cuánto tiempo (en promedio) ha durado cada puesto de trabajo
+    // en los registros de experiencia laboral.
+    //2. Te dice qué puestos suelen durar más tiempo, lo cual puede reflejar estabilidad
+    // laboral, madurez del cargo o nivel jerárquico.
     @Query(value = "SELECT\n" +
             "    job,\n" +
             "    AVG( (end_date - start_date) / (365.25 / 12.0) ) AS duracion_promedio_meses\n" +
@@ -25,7 +28,8 @@ public interface IExperienciaRepository extends JpaRepository<Experiencia, Integ
             "    duracion_promedio_meses DESC;",nativeQuery = true)
     public List<String[]> DuracionPromedioPorPuesto();
 
-    //Identifica si los usuarios tienen perfiles junior, intermedios o senior.
+    //1. Este query cuenta cuántas experiencias laborales tiene cada usuario.
+    //2. Identifica si los usuarios tienen perfiles junior, intermedios o senior.
     @Query(value = "SELECT \n" +
             "    c.id_usuario, \n" +
             "    COUNT(*) AS cantidad_experiencias\n" +
@@ -35,8 +39,4 @@ public interface IExperienciaRepository extends JpaRepository<Experiencia, Integ
             "GROUP BY c.id_usuario\n" +
             "ORDER BY cantidad_experiencias DESC;",nativeQuery = true)
     public List<String[]> PromedioExperienciaLaboral();
-
-
-
-
 }
